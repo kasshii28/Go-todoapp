09abc952fde0fe3199833e078237e4113cf5857a
kasshii28 <gp22a043@oecu.jp>
2024-04-28 15:04:27 +0900

    todoの取得

--- app/models/todos.go
+++ app/models/todos.go
@@ -65,3 +65,39 @@ func GetTodos() (todos []Todo, err error) {
 	return todos, err
 }
 
+//特定のユーザーのtodo取得
+func (u *User) GetTodosByUser() (todos []Todo, err error) {
+	cmd := `select id, content, user_id, created_at from todos
+	where user_id = ?`
+
+	rows, err := Db.Query(cmd, u.ID)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	for rows.Next(){
+		var todo Todo
+		err = rows.Scan(
+			&todo.ID,
+			&todo.Content,
+			&todo.UserID,
+			&todo.CreatedAt)
+
+		if err != nil {
+			log.Fatalln(err)
+		}
+		todos = append(todos, todo)
+	}
+	rows.Close()
+
+	return todos, err
+}
+
+//todoの更新
+func (t *Todo) UpdateTodo() error {
+	cmd := `update todos set content = ?, user_id = ? where id = ?`
+	_, err = Db.Exec(cmd, t.Content, t.UserID, t.ID)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	return err
+}
--- main.go
+++ main.go
@@ -6,24 +6,15 @@ import (
 )
 
 func main() {
-	/*
-		fmt.Println(config.Config.Port)
-		fmt.Println(config.Config.SQLDriver)
-		fmt.Println(config.Config.DbName)
-		fmt.Println(config.Config.LogFile)
-
-		log.Println("test")
-	*/
-
 	fmt.Println(models.Db)
 
-	// u := &models.User{}
-	// u.Name = "test"
-	// u.Email = "test@example.com"
-	// u.PassWord = "testtest"
-	// fmt.Println(u)
+	u := &models.User{}
+	u.Name = "test3"
+	u.Email = "test3@example.com"
+	u.PassWord = "testtest"
+	fmt.Println(u)
 
-	// u.CreateUser()
+	u.CreateUser()
 	
 
 	// u, _ := models.GetUser(1)
@@ -40,14 +31,24 @@ func main() {
 	// u, _ = models.GetUser(1)
 	// fmt.Println(u)
 
-	// user, _ := models.GetUser(2)
-	// user.CreateTodo("First Todo")
+	// user, _ := models.GetUser(3)
+	// user.CreateTodo("Third Todo")
 
 	// t, _ := models.GetTodo(1)
 	// fmt.Println(t)
 
-	todos, _ := models.GetTodos()
-	for _, v := range todos {
-		fmt.Println(v)
-	}
+	// todos, _ := models.GetTodos()
+	// for _, v := range todos {
+	// 	fmt.Println(v)
+	// }
+
+	// user2, _ := models.GetUser(3)
+	// todos, _ := user2.GetTodosByUser()
+	// for _, v := range todos {
+	// 		fmt.Println(v)
+	// }
+
+	t, _ := models.GetTodo(1)
+	t.Content = "Update Todo"
+	t.UpdateTodo()
 }
